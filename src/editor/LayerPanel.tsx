d6c8f58b169bd4bb77d634ee2c73403769a04890
Emil Widlund <hello@emilwidlund.com>
2024-11-18 17:44:07 +0100

    add more models

--- src/editor/LayerPanel.tsx
+++ src/editor/LayerPanel.tsx
@@ -1,11 +1,28 @@
-export interface Layer {
-  id: string;
-  name: string;
-  visible: boolean;
+import { Layer } from "@/models/layer";
+
+export interface LayerPanelProps {
+  layers: Layer[];
 }
 
-export interface LayerPanelProps {}
+export const LayerPanel = ({ layers }: LayerPanelProps) => {
+  return (
+    <div className="flex flex-col">
+      {layers.map((layer) => (
+        <LayerItem key={layer.id} layer={layer} />
+      ))}
+    </div>
+  );
+};
 
-export const LayerPanel = () => {
-  return <div></div>;
+export interface LayerItemProps {
+  layer: Layer;
+}
+
+export const LayerItem = ({ layer }: LayerItemProps) => {
+  return (
+    <div className="flex items-center">
+      <input type="checkbox" checked={layer.visible} />
+      <span>{layer.name}</span>
+    </div>
+  );
 };
